a4cd9be9380c15975008f1605b6ddf21c905395b
Konrad Michalik <km@move-elevator.de>
2026-04-27 13:14:22 +0200

    fix: register JavaScript module for theme preview

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'dependencies' => [
+        'backend',
+        'core',
+    ],
+    'imports' => [
+        '@konradmichalik/backend-themes/' => 'EXT:backend_themes/Resources/Public/JavaScript/',
+    ],
+];
