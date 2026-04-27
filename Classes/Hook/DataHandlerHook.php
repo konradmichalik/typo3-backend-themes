433f3df4a1507e6ba5f8425ea78ce23eeb35bc21
Konrad Michalik <km@move-elevator.de>
2026-04-27 11:12:36 +0200

    feat: add DataHandler hook to enforce single default theme

---
+++ Classes/Hook/DataHandlerHook.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "backend_themes" TYPO3 CMS extension.
+ *
+ * (c) 2026 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KonradMichalik\Typo3BackendThemes\Hook;
+
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\DataHandling\DataHandler;
+
+final readonly class DataHandlerHook
+{
+    private const TABLE_NAME = 'tx_backendthemes_theme';
+
+    public function __construct(private ConnectionPool $connectionPool) {}
+
+    /** @param array<string, mixed> $fieldArray */
+    public function processDatamap_afterDatabaseOperations(
+        string $status,
+        string $table,
+        int|string $id,
+        array &$fieldArray,
+        DataHandler $dataHandler,
+    ): void {
+        if ($table !== self::TABLE_NAME) {
+            return;
+        }
+
+        if (!isset($fieldArray['is_default']) || $fieldArray['is_default'] !== 1) {
+            return;
+        }
+
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable(self::TABLE_NAME);
+        $queryBuilder
+            ->update(self::TABLE_NAME)
+            ->set('is_default', 0)
+            ->where(
+                $queryBuilder->expr()->eq('is_default', $queryBuilder->createNamedParameter(1, Connection::PARAM_INT)),
+                $queryBuilder->expr()->neq('uid', $queryBuilder->createNamedParameter($id, Connection::PARAM_INT))
+            )
+            ->executeStatement();
+    }
+}
---
+++ Tests/Unit/Hook/DataHandlerHookTest.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "backend_themes" TYPO3 CMS extension.
+ *
+ * (c) 2026 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KonradMichalik\Typo3BackendThemes\Tests\Unit\Hook;
+
+use Doctrine\DBAL\Query\QueryBuilder as DoctrineQueryBuilder;
+use KonradMichalik\Typo3BackendThemes\Hook\DataHandlerHook;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder;
+use TYPO3\CMS\Core\DataHandling\DataHandler;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+
+final class DataHandlerHookTest extends TestCase
+{
+    private ConnectionPool&MockObject $connectionPool;
+    private DataHandlerHook $subject;
+
+    protected function setUp(): void
+    {
+        $this->connectionPool = $this->createMock(ConnectionPool::class);
+        $this->subject = new DataHandlerHook($this->connectionPool);
+    }
+
+    #[Test]
+    public function afterDatabaseOperationsResetsOtherDefaultsWhenSettingDefault(): void
+    {
+        $expressionBuilder = $this->createMock(ExpressionBuilder::class);
+        $expressionBuilder->method('eq')->willReturn('is_default = 1');
+        $expressionBuilder->method('neq')->willReturn('uid != 5');
+
+        $queryBuilder = $this->createMock(QueryBuilder::class);
+        $queryBuilder->method('update')->willReturnSelf();
+        $queryBuilder->method('set')->willReturnSelf();
+        $queryBuilder->method('where')->willReturnSelf();
+        $queryBuilder->method('expr')->willReturn($expressionBuilder);
+        $queryBuilder->method('createNamedParameter')->willReturnArgument(0);
+        $queryBuilder->expects(self::once())->method('executeStatement');
+
+        $this->connectionPool
+            ->expects(self::once())
+            ->method('getQueryBuilderForTable')
+            ->with('tx_backendthemes_theme')
+            ->willReturn($queryBuilder);
+
+        $dataHandler = $this->createMock(DataHandler::class);
+        $fieldArray = ['is_default' => 1];
+
+        $this->subject->processDatamap_afterDatabaseOperations(
+            'update',
+            'tx_backendthemes_theme',
+            5,
+            $fieldArray,
+            $dataHandler,
+        );
+    }
+
+    #[Test]
+    public function afterDatabaseOperationsSkipsWhenNotSettingDefault(): void
+    {
+        $this->connectionPool
+            ->expects(self::never())
+            ->method('getQueryBuilderForTable');
+
+        $dataHandler = $this->createMock(DataHandler::class);
+        $fieldArray = ['title' => 'My Theme'];
+
+        $this->subject->processDatamap_afterDatabaseOperations(
+            'update',
+            'tx_backendthemes_theme',
+            5,
+            $fieldArray,
+            $dataHandler,
+        );
+    }
+
+    #[Test]
+    public function afterDatabaseOperationsSkipsForOtherTables(): void
+    {
+        $this->connectionPool
+            ->expects(self::never())
+            ->method('getQueryBuilderForTable');
+
+        $dataHandler = $this->createMock(DataHandler::class);
+        $fieldArray = ['is_default' => 1];
+
+        $this->subject->processDatamap_afterDatabaseOperations(
+            'update',
+            'pages',
+            5,
+            $fieldArray,
+            $dataHandler,
+        );
+    }
+}
