af2aa41b1437d1f559de9c690c98de1531ada79a
Konrad Michalik <km@move-elevator.de>
2026-04-28 12:00:32 +0200

    fix: php cs fixer header rule

--- packaging_exclude.php
+++ packaging_exclude.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the "typo3_login_warning" TYPO3 CMS extension.
+ * This file is part of the "typo3_backend_themes" TYPO3 CMS extension.
  *
- * (c) 2025-2026 Konrad Michalik <km@move-elevator.de>
+ * (c) 2026 Konrad Michalik <hej@konradmichalik.dev>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
